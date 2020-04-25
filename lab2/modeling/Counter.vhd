60203628d37b075bb501f0a91e7b9750e909e00c
le0nidus <liran3t@gmail.com>
2020-04-25 18:52:20 +0300

    fixed counter.vhd

--- lab2/modeling/Counter.vhd
+++ lab2/modeling/Counter.vhd
@@ -16,42 +16,42 @@ end Counter;
 ------------- Counter Architecture code --------------
 architecture arc_Counter of Counter is
 SIGNAL tmpResult: STD_LOGIC_VECTOR(k-1 DOWNTO 0); 
-SIGNAL count : STD_LOGIC_VECTOR (7 DOWNTO 0)  ;	
 begin
 	counterProc :process (clk,rst,ena)
+		VARIABLE countVar: STD_LOGIC_VECTOR(7 DOWNTO 0);
 		begin
 			if(rst='1') then
-				count <= "00000001";
+				countVar := "00000001";
 				tmpResult <= (others => '0');
 			elsif (rising_edge(clk)) then	
 				IF(ena = '1') THEN
 					IF(riseSig = '1') THEN
-							count <= count( 6 downto 0 )&'0';
-							if(count = "00000010") then							
+							countVar := countVar( 6 downto 0 )&'0';
+							if(countVar = "00000010") then							
 								tmpResult <= "001";
-							elsif(count = "00000100") then
+							elsif(countVar = "00000100") then
 								tmpResult <= "010";
-							elsif(count = "00001000") then
+							elsif(countVar = "00001000") then
 								tmpResult <= "011";
-							elsif(count = "00001000") then
+							elsif(countVar = "00001000") then
 								tmpResult <= "100";
-							elsif(count = "00010000") then
+							elsif(countVar = "00010000") then
 								tmpResult <= "101";
-							elsif(count = "00100000") then
+							elsif(countVar = "00100000") then
 								tmpResult <= "110";
-							elsif(count = "01000000") then
+							elsif(countVar = "01000000") then
 								tmpResult <= "111";							
-							elsif(count = "10000000") THEN
+							elsif(countVar = "10000000") THEN
 								tmpResult<="111";
-								count<="01000000";
+								countVar := "01000000";
 							end if;				
 					ELSE 
-						count <= "00000001";
+						countVar := "00000001";
 						tmpResult<="000";
 					end IF;
 				ELSE
 					tmpResult<=(others => '0');
-					count <= "00000001";
+					countVar := "00000001";
 				end IF;
 			end IF;
 		END PROCESS counterProc;			
