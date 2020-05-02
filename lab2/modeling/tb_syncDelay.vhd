4d7291d43ac500fb6b2d1c5a99039fbb22dfd122
VladyslavKozhukhov <vladkozhuhov@gmail.com>
2020-05-02 17:05:26 +0300

    delay\ tb

--- lab2/modeling/tb_syncDelay.vhd
+++ lab2/modeling/tb_syncDelay.vhd
@@ -2,26 +2,22 @@
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_arith.all;
 USE ieee.std_logic_unsigned.all;
-USE work.aux_package.all;
+USE work.aux_package_delay.all;
 
-ENTITY top_tb IS
-	CONSTANT m : INTEGER := 8;
-END top_tb;
 
-architecture top_Testbench OF top_tb IS
+ENTITY delay_tb IS
+	CONSTANT n : INTEGER := 8;
+
+END delay_tb;
+
+architecture Delay_Testbench OF delay_tb IS
 SIGNAL rst,ena,clk: STD_LOGIC;
-	SIGNAL din: STD_LOGIC_VECTOR (m-1 DOWNTO 0);
-	SIGNAL cond : INTEGER range 0 to 3;
-	SIGNAL	detector : std_logic;
---	SIGNAL	X,Y : STD_LOGIC_VECTOR(8-1 DOWNTO 0);
-	--SIGNAL	riseSIGG:  std_logic;
---	SIGNAL	CR :  STD_LOGIC_VECTOR(3 DOWNTO 0);
-		 
-
+	SIGNAL	din :  std_logic_vector(n-1 downto 0);
+		SIGNAL mdin_i,din_iMinus :  std_logic_vector(n-1 downto 0);
 BEGIN
 
-	L0 : top PORT MAP(rst,ena,clk,din,cond,detector);
-				tb_ena : process
+	L0 : SynchronousDelay PORT MAP(rst,ena,clk,din,mdin_i,din_iMinus);
+		tb_ena : process
 			begin
 			ena<='0';
 			wait for 20 us;
@@ -30,7 +26,49 @@ BEGIN
 			
 			wait;
         end process tb_ena;
+	tb_rst : process
+			begin
+			rst<='1';
+			wait for 10 us;
+			rst<='0';			
+			wait;
+        end process tb_rst	;
 		
+        tb_clk : process
+			begin 
+			clk<='0';
+			wait for 5 us;
+			clk<='1';
+			wait for 5 us;
+        end process tb_clk;
 
-  
-END top_Testbench;
+ tb_din : process
+		begin
+			din <= "00000000";
+			wait for 10 us;
+			din <= "00000001";
+			wait for 10 us;
+			din <= "00000010";
+			wait for 10 us;
+			din <= "00000011"; 
+			wait for 10 us;
+			din <= "00000100";
+			wait for 10 us;
+			din <= "00000101";
+			wait for 10 us;
+			din <= "00000110";
+			wait for 10 us;
+			din <= "00000111";--7
+			wait for 10 us;
+			din <= "00001000";
+			wait for 10 us;
+			din <= "00001001";
+			wait for 10 us;
+			din <= "00001010";--10
+			wait for 10 us;
+			din <= "00001011";--11
+			wait for 10 us;	
+			
+  end process tb_din;
+
+END Delay_Testbench;
