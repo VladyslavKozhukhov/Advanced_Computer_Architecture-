575d750075d3815d10aa64a149f8a0ced96885f3
le0nidus <liran3t@gmail.com>
2020-05-14 11:48:42 +0300

    added continous reading in readFile.vhd

--- lab3/VHDL/readFile.vhd
+++ lab3/VHDL/readFile.vhd
@@ -11,6 +11,7 @@ ENTITY ReadLogic IS
 		m : POSITIVE := 5 -- OPC length
 	);
 	PORT (
+		clk : IN std_logic;
 		OPC : OUT std_logic_vector(m - 1 DOWNTO 0);
 		A, B : OUT std_logic_vector(n - 1 DOWNTO 0);
 		cin : OUT std_logic
@@ -19,8 +20,9 @@ END ReadLogic;
 ------------- ReadLogic Architecture code --------------
 ARCHITECTURE arc_ReadLogic OF ReadLogic IS
 	FILE file_VECTORS : text;
+
 BEGIN
-	readProc : PROCESS
+	readProc : PROCESS(clk)
 		VARIABLE v_ILINE : line;
 		VARIABLE v_A, v_B : std_logic_vector(n - 1 DOWNTO 0);
 		VARIABLE v_Cin : std_logic;
@@ -28,24 +30,26 @@ BEGIN
 		VARIABLE v_SPACEOne : CHARACTER;
 		VARIABLE v_SPACETwo : CHARACTER;
 		VARIABLE v_SPACEThree : CHARACTER;
-
 	BEGIN
-		file_open(file_VECTORS, "inputFile.txt", read_mode); -- op A B cin
-		WHILE NOT endfile(file_VECTORS) LOOP
-			readline(file_VECTORS, v_ILINE);
-			read(v_ILINE, v_OPC);
-			read(v_ILINE, v_SPACEOne);
-			read(v_ILINE, v_A);
-			read(v_ILINE, v_SPACETwo);
-			read(v_ILINE, v_B);
-			read(v_ILINE, v_SPACEThree);
-			read(v_ILINE, v_Cin);
-		END LOOP;
+		if (rising_edge(clk)) then
+			if NOT endfile(file_VECTORS) then
+				file_open(file_VECTORS, "inputFile.txt", read_mode); -- op A B cin
+				readline(file_VECTORS, v_ILINE);
+				read(v_ILINE, v_OPC);
+				read(v_ILINE, v_SPACEOne);
+				read(v_ILINE, v_A);
+				read(v_ILINE, v_SPACETwo);
+				read(v_ILINE, v_B);
+				read(v_ILINE, v_SPACEThree);
+				read(v_ILINE, v_Cin);
+			end if;
+			file_close(file_VECTORS);
+			WAIT;
+		end if;
 		OPC <= v_OPC;
 		A <= v_A;
 		B <= v_B;
 		cin <= v_Cin;
-		file_close(file_VECTORS);
-		WAIT;
 	END PROCESS readProc;
+	
 END arc_ReadLogic;
